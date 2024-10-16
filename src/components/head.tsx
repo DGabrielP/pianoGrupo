ae6ac4da3943da4c9c1a661e58d712d23324f52b
DGabrielP <dgpulgarin@sudamericano.edu.ec>
2024-10-16 14:55:14 -0500

    empece a definir el grid layaut en css

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -1,18 +1,25 @@
 
-import { Link } from 'react-router-dom'; // Usamos react-router-dom para navegar entre las páginas
-import './head'
-
+// head.tsx
+import { Link } from 'react-router-dom'; // Para la navegación entre las páginas
+import './head.css'; // Importa los estilos de CSS
 
 export default function Head() {
   return (
-    <header style={{ padding: '1em', backgroundColor: '#f8f9fa', borderBottom: '1px solid #dee2e6' }}>
-      <h1>Título del Sitio</h1>
-      <nav>
-        <ul style={{ listStyle: 'none', display: 'flex', gap: '1em' }}>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/game">Jugar</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <div className="header">
+      <div className="leftH">
+        <h1>Piano Kawi</h1>
+      </div>
+      <div className='centerH'>
+        <nav>
+          <ul className="nav-list">
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/game">Jugar</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <div className='rightH'>
+
+      </div>
+    </div>
   );
 }
