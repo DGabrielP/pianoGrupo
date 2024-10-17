17f34ffba5e6b4822832c5888107fac7c9ee5077
Jonnathan <jjcadme@sudamericano.edu.ec>
2024-10-17 18:34:05 -0500

    titulo
    Cambio de nombre Titulo

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -3,7 +3,7 @@ import './body'
 export default function Body() {
   return (
     <div style={{ border: '1px solid #ccc', padding: '2em', width: '600px', margin: '0 auto' }}>
-      <h2>Aquí irá el juego</h2>
+      <h2>Piano Virtual</h2>
       {/* Aquí puedes incrustar el código del juego */}
     </div>
   );
